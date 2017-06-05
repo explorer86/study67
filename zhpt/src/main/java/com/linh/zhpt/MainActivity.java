8c5e5a88cfcdd4de74425db48465b31049430b62
linhua <273896333@qq.com>
2017-06-05 22:31:33 +0800

    准备建立pager

--- zhpt/src/main/java/com/linh/zhpt/MainActivity.java
+++ zhpt/src/main/java/com/linh/zhpt/MainActivity.java
@@ -3,15 +3,20 @@ package com.linh.zhpt;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingFragmentActivity;
+import com.linh.zhpt.fragment.ContentFragment;
 
 /**
  * Created by ASUS on 2017/5/14.
  */
 
 public class MainActivity extends SlidingFragmentActivity {
+    private static final String TAG_CONTENT = "TAG_CONTENT";
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,5 +26,15 @@ public class MainActivity extends SlidingFragmentActivity {
         SlidingMenu slidingMenu = getSlidingMenu();
         slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
         slidingMenu.setBehindOffset(800);
+
+        initFragment();
+    }
+
+    private void initFragment() {
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.fl_main,new ContentFragment(),TAG_CONTENT);
+        transaction.commit();
     }
 }
--- zhpt/src/main/java/com/linh/zhpt/fragment/ContentFragment.java
+++ zhpt/src/main/java/com/linh/zhpt/fragment/ContentFragment.java
@@ -1,15 +1,26 @@
 package com.linh.zhpt.fragment;
 
+import android.support.v4.view.ViewPager;
 import android.view.View;
+import android.widget.RadioGroup;
+
+import com.linh.zhpt.R;
+
+import java.util.ArrayList;
 
 /**
  * Created by ASUS on 2017/5/21.
  */
 
 public class ContentFragment extends BaseFragment {
+    private ViewPager mViewPager;
+    private RadioGroup radioGroup;
+    //private ArrayList<BasePager>
     @Override
     public View initView() {
-        return null;
+        View view = View.inflate(mActivity, R.layout.fragment_content,null);
+
+        return view;
     }
 
     @Override
